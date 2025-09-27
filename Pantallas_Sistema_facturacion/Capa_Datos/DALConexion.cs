6ed867b1618883498d936f790759d0c2ef8824ff
Juanj15 <juanjimenez06@outlook.com>
2025-09-26 23:02:51 -0500

    chore: agrega .gitignore para ignorar configs sensibles

--- Pantallas_Sistema_facturacion/Capa_Datos/DALConexion.cs
+++ Pantallas_Sistema_facturacion/Capa_Datos/DALConexion.cs
@@ -5,7 +5,7 @@ namespace Capa_Datos
 {
     public class DALConexion
     {
-        ***REMOVED***
+        private SqlConnection Conexion = new SqlConnection("Server=tcp:srv-winforms-g7.database.windows.net,1433;Initial Catalog=DB-facturacion-g7;Persist Security Info=False;User ID=admin-g7;Password=!myA5x5WmSTqp8W;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
 
         public SqlConnection OpenConnection()
         {
