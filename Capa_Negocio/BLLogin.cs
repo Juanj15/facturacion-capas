ee2b8d10831c9def98552ca4ce127e7b35500422
Code Laura <lauragarcia19999@gmail.com>
2025-10-01 20:16:09 -0500

    login listo

---
+++ Capa_Negocio/BLLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Datos;
+
+namespace Capa_Negocio
+{
+    public class BLLogin
+    {
+        private DALLogin dal = new DALLogin();
+
+        public bool Login(string usuario, string contraseña)
+        {
+            if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(contraseña))
+                return false;
+
+            return dal.ValidarLogin(usuario, contraseña);
+        }
+    }
+}
